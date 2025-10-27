750404a3b171edf0e2586dc130a8ccbe3f20de7a
notnibloo <notnibloo@gmail.com>
2025-10-27 18:30:04 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/scripts',
+    '@nuxt/test-utils'
+  ]
+})
